aa4644d6abf60fc4230d49b74a855e4a221bc4af
Nitya Anuga <93179435+iamnitya@users.noreply.github.com>
2022-03-09 14:37:19 +0530

    Create 791A- Bear and Big Brother.py

---
+++ 791A- Bear and Big Brother/791A- Bear and Big Brother.py
@@ -0,0 +1,11 @@
+weights=input()
+weigts_split=weights.split(" ")
+Limak=int(weigts_split[0])
+Bob=int(weigts_split[1])
+n=1
+while((Limak*3)<=(Bob*2)):
+    pass
+    n=n+1
+    Limak=Limak*3
+    Bob=Bob*2
+print(n)
